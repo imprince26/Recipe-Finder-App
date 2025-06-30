fcb295753e4f4c8789ea9e763178f856063aa814
imprince26 <princepatel260806@gmail.com>
2025-06-30 14:26:28 +0530

    initial commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import neon from "@neondatabase/serverless";
+import {neon} from "@neondatabase/serverless";
 import "dotenv/config";
 import * as schema from "../db/schema.js";
 
